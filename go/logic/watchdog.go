f32bf3a1e81b8d3d3d4520f9413b80c4c1dcc21a
Tim Vaillancourt <tim@timvaillancourt.com>
2024-01-16 02:43:58 +0100

    Ignore temporary DNS errors up to a maximum
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- go/logic/watchdog.go
+++ go/logic/watchdog.go
@@ -3,6 +3,8 @@ package logic
 import (
 	gosql "database/sql"
 	"errors"
+	"net"
+	"sync/atomic"
 	"time"
 
 	"github.com/openark/golib/log"
@@ -12,9 +14,12 @@ import (
 )
 
 var (
-	ErrWatchdogCheckFailed      = errors.New("watchdog check failed")
-	ErrWatchdogUnexpectedChange = errors.New("watchdog detected unexpected change")
-	watchdogInterval            = time.Second * 10
+	ErrWatchdogCheckFailed             = errors.New("watchdog check failed")
+	ErrWatchdogTempDNSFailuresExceeded = errors.New("watchdog reached max temporary DNS failures")
+	ErrWatchdogUnexpectedChange        = errors.New("watchdog detected unexpected change")
+	//
+	maxTempDNSFailures int64 = 10
+	watchdogInterval         = time.Second * 10
 )
 
 type dbProvider interface {
@@ -32,6 +37,7 @@ type Watchdog struct {
 	migrationContext   *base.MigrationContext
 	serverInfoProvider func(dbProvider) (*mysql.ServerInfo, error)
 	done               chan bool
+	tempDNSFailures    map[string]*int64
 }
 
 func NewWatchdog(migrator *Migrator) *Watchdog {
@@ -42,20 +48,37 @@ func NewWatchdog(migrator *Migrator) *Watchdog {
 		},
 		migrationContext:   migrator.migrationContext,
 		serverInfoProvider: getDBProviderServerInfo,
+		tempDNSFailures:    make(map[string]*int64),
 	}
 }
 
 func (this *Watchdog) Teardown() {
 	close(this.done)
+	for k := range this.tempDNSFailures {
+		delete(this.tempDNSFailures, k)
+	}
 }
 
 func (this *Watchdog) checkDBProvider(provider dbProvider) error {
 	origServerInfo := provider.ServerInfo()
+	providerTempDNSFailures := this.tempDNSFailures[provider.Name()]
 	serverInfo, err := this.serverInfoProvider(provider)
 	if err != nil {
+		var dnsErr *net.DNSError
+		if errors.As(err, &dnsErr) {
+			if atomic.LoadInt64(providerTempDNSFailures) > maxTempDNSFailures {
+				log.Errorf("watchdog %s reached max temporary DNS failures (%d)", provider.Name(), maxTempDNSFailures)
+				return ErrWatchdogTempDNSFailuresExceeded
+			} else if dnsErr.IsTemporary {
+				log.Warningf("watchdog %s ignoring temporary DNS failure: %+v", provider.Name(), dnsErr.Err)
+				atomic.AddInt64(providerTempDNSFailures, 1)
+				return nil
+			}
+		}
 		log.Errorf("watchdog %s check failed: %+v", provider.Name(), err)
 		return ErrWatchdogCheckFailed
 	}
+	atomic.StoreInt64(providerTempDNSFailures, 0)
 	if !origServerInfo.Equals(serverInfo) {
 		log.Errorf("watchdog detected unexpected %s change from %+v to %+v", provider.Name(), origServerInfo, serverInfo)
 		return ErrWatchdogUnexpectedChange
@@ -75,6 +98,7 @@ func (this *Watchdog) InitiateChecker() {
 			for _, provider := range this.dbProviders {
 				if err := this.checkDBProvider(provider); err != nil {
 					this.migrationContext.PanicAbort <- err
+					return
 				}
 			}
 		}
